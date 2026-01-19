41ec036636fd990739eb022d414cd5ace99599e7
tacheraSasi <tacherasasi@gmail.com>
2026-01-20 00:11:30 +0300

    feat(notifications): add ExpoPushToken to TypeOrmModule imports

--- apps/backend/src/modules/notifications/notifications.module.ts
+++ apps/backend/src/modules/notifications/notifications.module.ts
@@ -4,9 +4,10 @@ import { NotificationsService } from './services/notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { Notification } from './entities/notification.entity';
 import { PushService } from './services/push.service';
+import { ExpoPushToken } from '../users/entities/expo-push-token.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [TypeOrmModule.forFeature([Notification,ExpoPushToken])],
   controllers: [NotificationsController],
   providers: [NotificationsService, PushService],
   exports: [NotificationsService, PushService, TypeOrmModule],
