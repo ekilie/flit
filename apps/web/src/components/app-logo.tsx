3dd0af8e2c7a57df7efda2ccdaf0cd56eed4e432
tacheraSasi <tacherasasi@gmail.com>
2026-02-26 16:39:59 +0300

    feat: add Logo component and implement landing page layout

---
+++ apps/web/src/components/app-logo.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils";
+
+export const Logo = ({ className }: { className?: string }) => {
+  return (
+    <div className={cn("flex items-center space-x-2", className)}>
+      <img src="/icon-512.png" alt="FLIT" width={45} height={45} />
+      {/* <span className="text-xl font-bold">FLIT</span> */}
+    </div>
+  );
+};
